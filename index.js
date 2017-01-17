1c69ba1f41d4f9c28e9f574003a11c5ee9a90633
Nick Matantsev <nick.matantsev@gmail.com>
2017-01-17 14:34:11 -0500

    Import code from unframework/react-interactable scratchpad

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    Focusable: require('./lib/Focusable'),
+    Hoverable: require('./lib/Hoverable'),
+    Movable: require('./lib/Movable'),
+    Pressable: require('./lib/Pressable'),
+
+    Linkable: require('./lib/Linkable'),
+
+    Expirable: require('./lib/Expirable'),
+    Fadeable: require('./lib/Fadeable'),
+    Holdable: require('./lib/Holdable'),
+
+    Activation: require('./lib/Activation'),
+    GroupState: require('./lib/GroupState'),
+    Notice: require('./lib/Notice'),
+    Queue: require('./lib/Queue'),
+    Submittable: require('./lib/Submittable'),
+    Toggleable: require('./lib/Toggleable'),
+    Triggerable: require('./lib/Triggerable')
+};
