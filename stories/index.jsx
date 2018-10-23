b5cdcb7bec79493723659689fd8c854a1d0747b6
Nick Matantsev <nick.matantsev@gmail.com>
2018-10-22 20:46:37 -0400

    Basic story for Timeout component

--- stories/index.jsx
+++ stories/index.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
+import Timeout from '!babel-loader!../lib/Timeout';
+
 storiesOf('react-dynamics', module)
-    .add('basic sequence', () => (
-        <div>Hello Storybook</div>
-    ));
+    .add('Timeout', () => {
+        const reportOuterRender = action('render parent');
+        const reportRender = action('render with timeout state');
+        const reportFinish = action('timeout then-prop call');
+
+        reportOuterRender();
+
+        return <div>
+            <span>Timeout (500ms): </span>
+            <Timeout on delayMs={500} then={() => {
+                reportFinish();
+            }}>{timeoutState => {
+                reportRender(timeoutState);
+
+                return timeoutState
+                    ? '...waiting'
+                    : 'finished!';
+            }}</Timeout>
+        </div>;
+    });
