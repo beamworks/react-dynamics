2722369cfaee8bf50608f72bde7e573782eaab28
Nick Matantsev <nick.matantsev@gmail.com>
2018-10-22 20:46:16 -0400

    Use the Storybook actions addon

--- stories/index.jsx
+++ stories/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('react-dynamics', module)
     .add('basic sequence', () => (
