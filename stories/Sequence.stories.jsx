1d318a17d8da2b46a5300619270abed09d57f324
Nick Matantsev <nick.matantsev@gmail.com>
2018-11-07 00:04:34 -0500

    Start working on a route-based sequence use case

--- stories/Sequence.stories.jsx
+++ stories/Sequence.stories.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route, MemoryRouter } from 'react-router';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Task from '!babel-loader!../lib/Task';
+import Op from '!babel-loader!../lib/Op';
 import Sequence from '!babel-loader!../lib/Sequence';
 
 storiesOf('Sequence', module)
@@ -40,4 +42,51 @@ storiesOf('Sequence', module)
             reportStepYield(step);
             return `FINAL STEP ${step}`;
         }}</Sequence>;
+    })
+    .add('routes + breadcrumbs', () => {
+        const reportStepYield = action('yielding on step');
+        const reportStepAnswer = action('resulting answer');
+
+        return <MemoryRouter><Op action={v => v}>{(currentOp, lastOp) => <Sequence
+            step={(prompt, next) => {
+                if (!next) {
+                    return <div>Finish {prompt}</div>;
+                }
+
+                const [ stepKey, stepContents ] = prompt;
+
+                return <Route path={`/${stepKey}`}>{({ match, location, history }) => {
+                    const allValues = lastOp && !lastOp.isError && lastOp.value || {};
+                    const stepValue = allValues[stepKey];
+
+                    if (match) {
+                        return <div>Step {stepContents} [{stepValue || '<empty>'}] | <button
+                            type="button"
+                            onClick={() => currentOp.invoke({ ...allValues, [stepKey]: `answer for ${stepContents}` })}
+                        >NEXT</button></div>;
+                    }
+
+                    if (!allValues.hasOwnProperty(stepKey)) {
+                        // stale navigation, redirect to start
+                        return <div><button type="button" onClick={() => history.push('/step1')}>Go to start</button></div>;
+                    }
+
+                    return next(stepValue);
+                }}</Route>;
+            }}
+        >{function* () {
+            let step = 1;
+
+            while (step < 3) {
+                reportStepYield(step);
+
+                const answer = yield [ `step${step}`, `STEP ${step}` ];
+                reportStepAnswer(answer);
+
+                step += 1;
+            }
+
+            reportStepYield(step);
+            return `FINAL STEP ${step}`;
+        }}</Sequence>}</Op></MemoryRouter>;
     });
