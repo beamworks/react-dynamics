51558ad1e70cf826a11069103e8a8a318f11677b
Nick Matantsev <nick.matantsev@gmail.com>
2018-11-06 21:58:06 -0500

    Add basic sequence story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
+    require('../stories/Sequence.stories.jsx');
     require('../stories/Op.stories.jsx');
     require('../stories/Timeout.stories.jsx');
 }
