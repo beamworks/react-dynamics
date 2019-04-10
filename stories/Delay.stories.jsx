de713f7ffe1b9ff9a2ad0a22012be5c72522576f
Nick Matantsev <nick.matantsev@gmail.com>
2019-04-10 15:34:16 -0400

    Change Delay to use observed-props and disabled flag

--- stories/Delay.stories.jsx
+++ stories/Delay.stories.jsx
@@ -14,7 +14,7 @@ storiesOf('Delay', module)
 
         return <div>
             <span>Delay (500ms): </span>
-            <Delay on delayMs={500} onComplete={() => {
+            <Delay delayMs={500} onComplete={() => {
                 reportFinish();
             }}>{timeoutState => {
                 reportRender(timeoutState);
@@ -22,6 +22,33 @@ storiesOf('Delay', module)
                 return timeoutState
                     ? '...waiting'
                     : 'finished!';
+            }}</Delay>
+        </div>;
+    })
+    .add('with disabled state', () => {
+        const reportOuterRender = action('render parent');
+        const reportRender = action('render with timeout state');
+        const reportFinish = action('timeout onComplete call');
+
+        reportOuterRender();
+
+        return <div>
+            <span>Delay (1s before start, then 2s): </span>
+            <Delay delayMs={1000}>{outerTimeoutState => {
+                // interrupt the first timeout via the observed-prop
+                return <Delay disabled={outerTimeoutState} delayMs={2000} onComplete={() => {
+                    reportFinish();
+                }}>{timeoutState => {
+                    reportRender(new Date(), timeoutState);
+
+                    if (outerTimeoutState) {
+                        return 'waiting to enable...';
+                    }
+
+                    return timeoutState
+                        ? '...enabled, waiting'
+                        : 'finished!';
+                }}</Delay>;
             }}</Delay>
         </div>;
     })
@@ -34,9 +61,9 @@ storiesOf('Delay', module)
 
         return <div>
             <span>Delay (500ms): </span>
-            <Delay on delayMs={500}>{outerTimeoutState => {
-                // interrupt the first timeout via the on-prop
-                return <Delay on={outerTimeoutState ? 'first' : 'second'} delayMs={1000} onComplete={() => {
+            <Delay delayMs={500}>{outerTimeoutState => {
+                // interrupt the first timeout via the observed-prop
+                return <Delay observe={outerTimeoutState} delayMs={1000} onComplete={() => {
                     reportFinish();
                 }}>{timeoutState => {
                     reportRender(timeoutState);
